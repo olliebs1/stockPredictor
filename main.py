6e198000ed77ee487b1fabab52b221fef6403f87
Olivier benson-smith <olliebenson@hotmail.co.uk>
2020-05-06 23:11:48 +0100

    code written and working for test model

--- main.py
+++ main.py
@@ -42,3 +42,10 @@ x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 # Create and train the support vector machine (Regressor)
 svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
 svr_rbf.fit(x_train, y_train)
+
+
+# Test model: Score returns the coefficient of determination R2 of the prediction
+# The best possible score is 1.0
+
+svm_confidence = svr_rbf.score(x_test, y_test)
+print('svm confidence:', svm_confidence)
