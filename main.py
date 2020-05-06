520f26240536f3b1b5df53b67daf9aba4e39daa5
Olivier benson-smith <olliebenson@hotmail.co.uk>
2020-05-06 21:52:46 +0100

    basic variables created to retrieve data and basic 1 day prediction

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split
 
+# Take a look at the data
 df = quandl.get("WIKI/FB", api_key=apiKey)
+# print(df.head())
 
+# Get the adjusted lose price
+df = df[['Adj. Close']]
+
+# A variable for predicting 'n' days into the future
+forecast_out = 1
+
+# Create another column (the target or dependent variable) shifted 'n' units up
+df['Prediction'] = df[['Adj. Close']].shift(-1)
 print(df.head())
